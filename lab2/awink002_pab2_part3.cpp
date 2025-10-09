54562484e42d18c58aa09e0623c1f2bf0541e6fd
prestofuoco <awink002@ucr.edu>
2025-10-09 14:02:47 -0700

    feat: Merge lab2 into subdirectory

---
+++ lab2/awink002_pab2_part3.cpp
@@ -0,0 +1,107 @@
+/*        
+Your Name & E-mail: Andre Winkel, awink002@ucr.edu
+Discussion Section: 024
+Assignment: Lab #2  Exercise #3
+Exercise Description: 7seg and led counter
+I acknowledge all content contained herein, excluding template or example code, is my own original work.
+Demo Link: <URL>
+*/
+
+#include <avr/io.h>
+
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+   return (b ?  (x | (0x01 << k))  :  (x & ~(0x01 << k)) );
+}
+
+unsigned char GetBit(unsigned char x, unsigned char k) {
+   return ((x & (0x01 << k)) != 0);
+}
+
+// the index is also the digit it represents
+// eg: nums[0]="0", nums[9]="9", nums[15]="f"
+// bits layout:   gfedcba   (REVERSE ORDER!)
+int nums[16] = {0b0111111, // 0
+                0b0000110, // 1
+                0b1011011, // 2
+                0b1001111, // 3
+                0b1100110, // 4
+                0b1101101, // 5
+                0b1111101, // 6
+                0b0000111, // 7
+                0b1111111, // 8
+                0b1101111, // 9
+                0b1110111, // A
+                0b1111100, // b
+                0b0111001, // C
+                0b1011110, // d
+                0b1111001, // E
+                0b1110001  // F
+                };
+
+//TODO: complete outNum()
+void outNum(int num) {
+    PORTB = (nums[num] & 0x1F);
+    PORTD = ((nums[num] & 0x60) << 1) | (num << 2);
+}
+
+unsigned int num = 0;
+enum states {hold, inc, dec, wait} state; //sm enum
+
+void Tick() {
+    //static vars
+
+    //state transitions
+    switch(state) {
+        case hold:
+            if ((PINC & 0x01)) {state = dec;}
+            else if ((PINC & 0x02)) {state = inc;}
+            else {state = hold;}
+            break;
+        case wait:
+            if ((!(PINC & 0x01)) && (!(PINC & 0x02))) {state = hold;} 
+            else {state = wait;}
+            break;
+        case inc:
+            state = wait;
+            break;
+        case dec:
+            state = wait;
+            break;
+        default: 
+            break;
+    }
+
+    //state actions
+    switch(state) {
+        case hold:
+            break;
+        case wait:
+            break;
+        case inc:
+            if ((num < 15)) {num++;}
+            else if ((num == 15)) {num = 0;}
+      		outNum(num);
+            break;
+        case dec:
+            if ((num > 0)) {num--;}
+            else if ((num == 0)) {num = 15;}
+      		outNum(num);
+            break;
+        default: 
+            break;
+    }
+}
+
+int main(void) {
+    //io initialization
+    DDRB = 0xFF; PORTB = 0x00; //outputs
+    DDRC = 0x00; PORTC = 0xFF; //inputs
+    DDRD = 0xFF; PORTD = 0x00; //outputs
+
+  	//state initialization
+    state = hold;
+
+    while (1) {
+        Tick();
+    }
+}
