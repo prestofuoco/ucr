7ccb2d7ba96523254488024d46a8c7453ed55867
asw <andrewinkel14@gmail.com>
2025-10-16 19:46:06 -0700

    l3p1 done, need to check with hw

--- ee120b/lab3/awink002_lab3_part1.cpp
+++ ee120b/lab3/awink002_lab3_part1.cpp
@@ -1,81 +1,69 @@
-/*        
-Your Name & E-mail: Andre Winkel, awink002@ucr.edu
+/* Your Name & E-mail: Andre Winkel, awink002@ucr.edu
 Discussion Section: 024
 Assignment: Lab #3 Exercise #1
-Exercise Description: observing adc values on serial monitor
+Exercise Description: reads potentiometer and outputs to serial
 I acknowledge all content contained herein, excluding template or example code, is my own original work.
-Demo Link: 
+Demo Link: YOUR_YOUTUBE_LINK_HERE
 */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "timerISR.h"
-
 #include "serialATmega.h"
 
-void TimerISR() {
-    TimerFlag = 0.5;
-}
-
 void ADC_init() {
-    // TODO: figure out register values
-    ADMUX = (1 << REFS0);
-    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    // ADMUX: Set REFS0=1 for AVCC reference voltage. 0b01000000
+    ADMUX = 0x40;
+    // ADCSRA: Set ADEN=1 to enable ADC, set ADPS2:0=111 for prescaler of 128. 0b10000111
+    ADCSRA = 0x87;
+    // ADCSRB: Leave as 0 for free-running mode. 0b00000000
     ADCSRB = 0x00;
   }
-  
-  
   
 unsigned int ADC_read(unsigned char chnl){
     //                              ^^^^ unsigned char chnl selects which pin you're reading analog signal from
   
-    ADMUX  = /*set MUX3:0 bits without modifying any other bits*/;
-    ADCSRA = /*set the bit that starts conversion in free running mode without modifying any other bit*/;
-    while((ADCSRA >> 6)&0x01) {} /*what does this line do?*/
+    ADMUX = (ADMUX & 0xF0) | (chnl & 0x0F);
+    ADCSRA |= 0x40;
+    
+    while((ADCSRA >> 6) & 0x01) {}  // wait for adsc
 
     uint8_t low, high;
 
-    low = 0;
-    high = 1024;
+    // Must read ADCL first, then ADCH.
+    low = ADCL;
+    high = ADCH;
 
-    return ((high << 8) | low);
+    return ((high << 8) | low) ;
 }
-
-enum States {Start} state;
 
 void Tick() {
-    switch(state) {
-        case Start: state = Start; break;
-        default: break;
-    };
-    switch(state) {
-        case Start: ADC_read(0x08); break;
-        default: break;
-    };
+    unsigned int pot_value = ADC_read(0);
+    serial_char(pot_value);
 }
 
-int main(void)
-{
-    // TODO: Initialize your I/O pins
-    DDRC = 0x00;
-    PORTC = 0xFF;
+int main(void) {
+    DDRC    = 0x00;
+    PORTC   = 0x00;
 
-    DDRB = 0xFF;
-    PORTB = 0x00;
+    DDRB    = 0xFF;
+    PORTB   = 0x00;
 
-    DDRD = 0xFF;
-    PORTD = 0x00;
+    DDRD    = 0xFF;
+    PORTD   = 0x00;
 
     ADC_init();
 
     serial_init(9600);
     
-    while (1)
-    {
-		  Tick();      // Execute one synchSM tick
-      while (!TimerFlag){}  // Wait for SM period
-      TimerFlag = 0;
+    TimerSet(500);
+    TimerOn();
+    
+    while (1) {
+        Tick();
+        while (!TimerFlag){}
+        TimerFlag = 0;
     }
 
     return 0;
--- ee120b/testbench/src/main.cpp
+++ ee120b/testbench/src/main.cpp
@@ -1,68 +1,69 @@
-/*        
-Your Name & E-mail: Andre Winkel, awink002@ucr.edu
+/* Your Name & E-mail: Andre Winkel, awink002@ucr.edu
 Discussion Section: 024
 Assignment: Lab #3 Exercise #1
-Exercise Description: 
+Exercise Description: reads potentiometer and outputs to serial
 I acknowledge all content contained herein, excluding template or example code, is my own original work.
-Demo Link: 
+Demo Link: YOUR_YOUTUBE_LINK_HERE
 */
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include "timerISR.h"
-
 #include "serialATmega.h"
 
 void ADC_init() {
-    // TODO: figure out register values
-    ADMUX = (1 << REFS0);
-    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    // ADMUX: Set REFS0=1 for AVCC reference voltage. 0b01000000
+    ADMUX = 0x40;
+    // ADCSRA: Set ADEN=1 to enable ADC, set ADPS2:0=111 for prescaler of 128. 0b10000111
+    ADCSRA = 0x87;
+    // ADCSRB: Leave as 0 for free-running mode. 0b00000000
     ADCSRB = 0x00;
   }
-  
-  
   
 unsigned int ADC_read(unsigned char chnl){
     //                              ^^^^ unsigned char chnl selects which pin you're reading analog signal from
   
-    ADMUX  = /*set MUX3:0 bits without modifying any other bits*/;
-    ADCSRA = /*set the bit that starts conversion in free running mode without modifying any other bit*/;
-    while((ADCSRA >> 6)&0x01) {} /*what does this line do?*/
+    ADMUX = (ADMUX & 0xF0) | (chnl & 0x0F);
+    ADCSRA |= 0x40;
+    
+    while((ADCSRA >> 6) & 0x01) {}  // wait for adsc
 
     uint8_t low, high;
 
-    low = /*what should this get assigned with?*/;
-    high = /*what should this get assigned with?*/;
+    // Must read ADCL first, then ADCH.
+    low = ADCL;
+    high = ADCH;
 
     return ((high << 8) | low) ;
 }
 
 void Tick() {
-    // TODO: Implement your Tick Function
+    unsigned int pot_value = ADC_read(0);
+    serial_char(pot_value);
 }
 
-int main(void)
-{
-    // TODO: Initialize your I/O pins
-    DDRC    = ;
-    PORTC   = ;
+int main(void) {
+    DDRC    = 0x00;
+    PORTC   = 0x00;
 
-    DDRB     = ;
-    PORTB    = ;
+    DDRB    = 0xFF;
+    PORTB   = 0x00;
 
-    DDRD   = ;
-    PORTD  = ;
+    DDRD    = 0xFF;
+    PORTD   = 0x00;
 
     ADC_init();
 
     serial_init(9600);
     
-    while (1)
-    {
-		  Tick();      // Execute one synchSM tick
-      while (!TimerFlag){}  // Wait for SM period
-      TimerFlag = 0;
+    TimerSet(500);
+    TimerOn();
+    
+    while (1) {
+        Tick();
+        while (!TimerFlag){}
+        TimerFlag = 0;
     }
 
     return 0;
